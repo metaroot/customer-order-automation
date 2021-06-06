db449cb1dba058c071a1dfc47cd7ed40d317075d
metaroot <faisal.ahmed.cse.sust@gmail.com>
2021-06-06 15:20:40 +0600

    Add plugin

--- src/main/java/com/cms/orderservices/customerorderautomation/CustomerOrderAutomationApplication.java
+++ src/main/java/com/cms/orderservices/customerorderautomation/CustomerOrderAutomationApplication.java
@@ -1,5 +1,6 @@
 package com.cms.orderservices.customerorderautomation;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
